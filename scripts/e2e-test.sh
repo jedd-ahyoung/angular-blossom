75b35bd9ac34f723ca528f0a41051ffb7becf6ce
James <james@thedigitalself.com>
2012-10-07 18:21:07 -0400

    added namespaces and modules. removed original js files and folders

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -3,7 +3,7 @@
 BASE_DIR=`dirname $0`
 
 echo ""
-echo "Starting Testacular Server (http://vojtajina.github.com/testacular)"
-echo "-------------------------------------------------------------------"
+echo "Starting Testacular E2E Server (http://vojtajina.github.com/testacular)"
+echo "-----------------------------------------------------------------------"
 
 testacular start $BASE_DIR/../config/testacular-e2e.conf.js $*
